3401a3e5ba94f46dec1a9ce222f858d663b84506
Egor Gorodov <egor.gorodov@gmail.com>
2015-07-24 12:38:25 +0300

    add session timestamp

--- lib/devinotele_rest.rb
+++ lib/devinotele_rest.rb
@@ -9,15 +9,28 @@ require 'devinotele_rest/sms'
 module DevinoteleRest
   BASE_REST_URL = 'https://integrationapi.net'
 
+  attr_reader :conn, :login, :password
+
   class Client
     def initialize(login, password)
+      @login = login
+      @password = password
       @conn = DevinoteleRest::Connection.create
-      @session_r = DevinoteleRest::Session.new(login, password, @conn)
     end
 
     def create(options)
-      session = @session_r.get_session
-      DevinoteleRest::Sms::Create.create(options, @conn, session)
+      @session = get_session
+      DevinoteleRest::Sms::Create.create(options, conn, @session.fetch(:token))
+    end
+
+    private
+    def get_session
+      @session_requester = DevinoteleRest::Session.new(login, password, conn)
+      if @session && @session_requester.valid_session?(@session)
+        @session
+      else
+        @session_requester.get_session
+      end
     end
   end
 end
--- lib/devinotele_rest/session.rb
+++ lib/devinotele_rest/session.rb
@@ -1,6 +1,7 @@
 module DevinoteleRest
   class Session
     URL = '/rest/user/sessionid?'
+    SESSION_LIFE_IN_MINUTES = 120
 
     def initialize(login, password, conn)
       @login = login
@@ -11,12 +12,26 @@ module DevinoteleRest
     def get_session
       res = @conn.get URL, { login: @login, password: @password }
       if res.success?
-        res.body.slice(1..36)
+        { token: res.body.slice(1..36), created_at: Time.now }
       else
         raise DevinoteleRest::RequestError, JSON.parse(res.body)['Desc']
       end
     rescue Faraday::Error::TimeoutError => e
       raise DevinoteleRest::RequestError, e.message
+    end
+
+    def valid_session?(session)
+      if session.is_a? Hash
+        created_at = session.fetch(:created_at)
+        (end_session_time(created_at) > Time.now) ? true : false
+      else
+        raise 'Session must be a hash'
+      end
+    end
+
+    private
+    def end_session_time(created_at)
+      created_at + SESSION_LIFE_IN_MINUTES * 60
     end
   end
 end
